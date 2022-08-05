4078e00630fe730201ac74884850339250067939
Nayan Patil <patil.nayan04@gmail.com>
2022-08-05 17:31:33 +0530

    Light/Dark mode symbol and error handling for api

--- Components/DarkModeSwitch.tsx
+++ Components/DarkModeSwitch.tsx
@@ -1,16 +1,23 @@
-import { useColorMode, Switch } from '@chakra-ui/react'
-
-export const DarkModeSwitch : React.FC = (props) => {
-  const { colorMode, toggleColorMode } = useColorMode()
-  const isDark = colorMode === 'dark'
+import { useColorMode, Switch, HStack, Icon } from "@chakra-ui/react";
+import { BsFillSunFill } from "react-icons/bs";
+import { FaMoon } from "react-icons/fa";
+export const DarkModeSwitch: React.FC = (props) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   return (
-    <Switch
-      right="1rem"
-      color="green"
-      isChecked={isDark}
-      {...props}
-      onChange={toggleColorMode}
-      
-    />
-  )
-}
+    <HStack spacing="8">
+      {colorMode === "light" ? (
+        <Icon color="white" fontSize="20px" as={BsFillSunFill} />
+      ) : (
+        <Icon color="white" fontSize="20px" as={FaMoon} />
+      )}
+      <Switch
+        right="1rem"
+        colorScheme="orange"
+        isChecked={isDark}
+        {...props}
+        onChange={toggleColorMode}
+      />
+    </HStack>
+  );
+};
--- Components/HomeSection.tsx
+++ Components/HomeSection.tsx
@@ -2,7 +2,7 @@ import { SearchIcon } from "@chakra-ui/icons";
 import {
   Box,
   Center,
-  color,
+  Text,
   Input,
   InputGroup,
   InputLeftElement,
@@ -22,9 +22,9 @@ type Props = {};
 const HomeSection = (props: Props) => {
   const searchInputBgColorMode = useColorModeValue("white", "#212122");
   const searchInputTextColorMode = useColorModeValue("black", "white");
-  const [searchResults, setSearchResults] = useState<Post[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState("")
+  const [searchResults, setSearchResults] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   const { colorMode } = useColorMode();
 
   const bgColor = { light: "#ebebeb", dark: "#030303" };
@@ -32,8 +32,6 @@ const HomeSection = (props: Props) => {
   const color = { light: "black", dark: "white" };
 
   let cancelToken: CancelTokenSource;
-
-
 
   const search = async (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -44,8 +42,7 @@ const HomeSection = (props: Props) => {
       setSearchResults([]);
       return;
     }
-    setIsLoading(true)
-
+    setIsLoading(true);
 
     if (typeof cancelToken !== typeof undefined) {
       cancelToken.cancel("Cancelling the previous req");
@@ -60,20 +57,22 @@ const HomeSection = (props: Props) => {
         { cancelToken: cancelToken.token }
       );
       // console.table(response.data)
-      
+
       if (response.status === 200) {
-        console.table(response.data)
+        console.table(response.data);
         setSearchResults(response.data.hits);
-        setIsLoading(false)
-      } 
+        setIsLoading(false);
+      }
     } catch (error) {
-      console.error(error)
+      console.error(error);
       // @ts-ignore
-      setError(error.message)
-      setIsLoading(false)
+      if (error.code !== "ERR_CANCELED") {
+        // @ts-ignore
+        setError(error.message);
+      }
+      setIsLoading(false);
     }
   };
-
 
   return (
     <VStack spacing="10" width="full">
@@ -97,27 +96,28 @@ const HomeSection = (props: Props) => {
           bgColor={searchInputBgColorMode}
         />
       </InputGroup>
-      {
-        isLoading ?   <Box
-        bg={bgColor[colorMode]}
-        color={color[colorMode]}
-        margin="2"
-        h="100vh"
-      >
-        
-        <Center height="full">
-          <Spinner size="lg" />
-        </Center>
-      </Box> : <VStack w="70vw" spacing={2} >
-        {
-          searchResults.map((post) => {
-            return <PostComponent post={post} key={post.objectID} /> 
-          })
-        }
-        
-      </VStack>
-      }
-      
+      {isLoading ? (
+        <Box
+          bg={bgColor[colorMode]}
+          color={color[colorMode]}
+          margin="2"
+          h="100vh"
+        >
+          <Center height="full">
+            <Spinner size="lg" />
+          </Center>
+        </Box>
+      ) : error ? (
+        <Text>Something went wrong, Please try again later!</Text>
+      ) : searchResults.length === 0 ? (
+        <Text>No Post at the moment !</Text>
+      ) : (
+        <VStack w="70vw" spacing={2}>
+          {searchResults.map((post) => {
+            return <PostComponent post={post} key={post.objectID} />;
+          })}
+        </VStack>
+      )}
     </VStack>
   );
 };
